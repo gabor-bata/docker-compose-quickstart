8d20b9ce68bb231860f605ff9c897646525e23a4
Gabor Bata <Gabor_Bata@epam.com>
2017-09-14 16:58:42 +0200

    Moved redis declaration out from request handler

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ require 'redis'
 set :port, 5000
 set :bind, '0.0.0.0'
 
+redis = Redis.new(host: 'redis', port: 6379)
+
 get '/' do
-  redis = Redis.new(host: 'redis', port: 6379)
   count = redis.incr 'hits'
   "Hello World! I have been seen #{count} times."
 end
